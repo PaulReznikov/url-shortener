6a8a4fdcd70ad8ee6f4e5953afea88738ecb2106
Paul Reznikov <m0thers0n9588@gmail.com>
2025-01-08 21:25:03 +0300

    init config && init storage method of saving url is implemented

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -2,8 +2,10 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq" //init postgres driver
+	"url-shortener/internal/storage"
 )
 
 type Storage struct {
@@ -15,40 +17,89 @@ func New(storagePath string) (*Storage, error) {
 
 	db, err := sql.Open("postgres", storagePath)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	//first query
 	stmt1, err := db.Prepare(`
 	CREATE TABLE IF NOT EXISTS url(
 	    id SERIAL PRIMARY KEY,
-	    alias TEXT NOT NULL UNIQUE,
+	    alias TEXT UNIQUE NOT NULL,
 	    url TEXT NOT NULL);
 	`)
 
 	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	defer stmt1.Close()
 
 	_, err = stmt1.Exec()
 	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	//second query
 	stmt2, err := db.Prepare(`CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);`)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	defer stmt2.Close()
 
 	_, err = stmt2.Exec()
 	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.postgres.SaveURL"
+
+	tx, err := s.db.Begin()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	stmt, err := tx.Prepare(`INSERT INTO url(alias, url)
+									VALUES($1, $2) RETURNING id`)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(alias, urlToSave)
+	if err != nil {
+		//TODO refactor this
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) && pqErr.Code == "23505" {
+			// Код 23505 означает нарушение уникального ограничения
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
 }
